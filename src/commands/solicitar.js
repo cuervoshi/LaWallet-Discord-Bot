fda7f5c9a26e8277d8a1497eb54bb7d1aaf18ec1
ferpa <feririarte.1996@gmail.com>
2024-09-03 12:14:41 -0300

    add /solicitar command & payment button

---
+++ src/commands/solicitar.js
@@ -0,0 +1,85 @@
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
+import { getOrCreateAccount } from "../handlers/accounts.js";
+import { EphemeralMessageResponse } from "../utils/helperFunctions.js";
+import { AuthorConfig } from "../utils/helperConfig.js";
+import { formatter } from "../utils/helperFormatter.js";
+
+// Creates an object with the data required by Discord's API to create a SlashCommand
+const create = () => {
+  const command = new SlashCommandBuilder()
+    .setName("solicitar")
+    .setDescription("Solicitar que te paguen una factura")
+    .addNumberOption((opt) =>
+      opt
+        .setName("monto")
+        .setDescription("La cantidad de satoshis a pagar en la factura")
+        .setRequired(true)
+    )
+    .addStringOption((opt) =>
+      opt
+        .setName("descripcion")
+        .setDescription("La descripción de la factura")
+        .setRequired(false)
+    );
+
+  return command.toJSON();
+};
+
+// Called by the interactionCreate event listener when the corresponding command is invoked
+const invoke = async (interaction) => {
+  try {
+    const user = interaction.user;
+    if (!user) return;
+
+    await interaction.deferReply();
+    const amount = parseInt(interaction.options.get(`monto`).value);
+    const description = interaction.options.get(`descripcion`);
+
+    if (amount <= 0)
+      return FollowUpEphemeralResponse(
+        interaction,
+        "No se permiten saldos negativos"
+      );
+
+    const wallet = await getOrCreateAccount(user.id);
+
+    const invoiceDetails = await wallet.generateInvoice({
+      milisatoshis: amount * 1000,
+      comment: description ? description.value : "",
+    });
+
+    const embed = new EmbedBuilder().setAuthor(AuthorConfig).addFields([
+      {
+        name: `Solicitud de pago`,
+        value: `${invoiceDetails.pr}`,
+      },
+      {
+        name: `monto (sats)`,
+        value: `${formatter(0, 0).format(amount)}`,
+      },
+    ]);
+
+    const row = new ActionRowBuilder().addComponents([
+      new ButtonBuilder()
+        .setCustomId("pay")
+        .setLabel("Pagar factura")
+        .setEmoji({ name: `💸` })
+        .setStyle(2),
+    ]);
+
+    interaction.editReply({
+      embeds: [embed],
+      components: [row],
+    });
+  } catch (err) {
+    console.log(err);
+    EphemeralMessageResponse(interaction, "Ocurrió un error");
+  }
+};
+
+export { create, invoke };
---
+++ src/components/buttons/pay.js
@@ -0,0 +1,63 @@
+import { ActionRowBuilder, ButtonBuilder } from "discord.js";
+import { getOrCreateAccount } from "../../handlers/accounts.js";
+
+const customId = "pay";
+
+const invoke = async (interaction) => {
+  try {
+    await interaction.deferReply({ ephemeral: true });
+    const payUrl = interaction.message.embeds[0].fields.find(
+      (field) => field.name === "Solicitud de pago"
+    );
+
+    const amountOnSats = interaction.message.embeds[0].fields.find(
+      (field) => field.name === "monto (sats)"
+    );
+
+    if (payUrl) {
+      const userWallet = await getOrCreateAccount(
+        interaction.user.id,
+        interaction.user.username
+      );
+
+      const mSatsBalance = await userWallet.getBalance("BTC");
+      const satsBalance = mSatsBalance * 1000;
+
+      if (satsBalance < amountOnSats.value) {
+        return FollowUpEphemeralResponse(
+          interaction,
+          `No tienes balance suficiente para pagar esta factura. \nTu balance: ${satsBalance} - Requerido: ${amountOnSats.value}`
+        );
+      } else {
+        await userWallet.payInvoice({
+          paymentRequest: payUrl.value,
+          onSuccess: async () => {
+            const row = new ActionRowBuilder().addComponents([
+              new ButtonBuilder()
+                .setCustomId("pay")
+                .setLabel(`Pagada por @${interaction.user.username}`)
+                .setEmoji({ name: `💸` })
+                .setStyle(2)
+                .setDisabled(true),
+            ]);
+
+            interaction.message.edit({ components: [row] });
+          },
+          onError: () => {
+            FollowUpEphemeralResponse(interaction, "Ocurrió un error");
+          },
+        });
+
+        return interaction.editReply({
+          content: "Interacción con pago de factura completada.",
+          ephemeral: true,
+        });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    return FollowUpEphemeralResponse(interaction, "Ocurrió un error");
+  }
+};
+
+export { invoke, customId };
